478b74f1489e03657bb233e2a512a1be4dc2f967
sara040616 <sara.sierraramos@gmail.com>
2023-08-25 22:03:51 -0500

    Ajustes en la carpeta assets y rutas de las imagenes

--- src/componentes/proyectos/Proyectos.jsx
+++ src/componentes/proyectos/Proyectos.jsx
@@ -15,10 +15,10 @@ export default function Proyectos() {
         <Titulo>Proyectos</Titulo>
         <div className='d-flex flex-wrap proyectos'>
           <Tarjetas href2='/' href1='https://github.com/SaraStefania' titulo='Portafolio' src='public/assets/portafolio.png'></Tarjetas>
-          <Tarjetas href2='https://dev-003-burger-queen-api-client-one.vercel.app/' href1='https://github.com/SaraStefania/DEV003-burger-queen-api-client' titulo='Burguer queen' src='public/assets/burguerImagen.png'></Tarjetas>
+          <Tarjetas href2='https://dev-003-burger-queen-api-client-one.vercel.app/' href1='https://github.com/SaraStefania/DEV003-burger-queen-api-client' titulo='Burguer queen' src='/assets/burguerImagen.png'></Tarjetas>
           <Tarjetas href2='www.npmjs.com/package/md-links-minder' href1='https://github.com/SaraStefania/DEV003-md-links' titulo='Md Links' src='public/assets/mdLinks.png'></Tarjetas>
-          <Tarjetas href2='https://sarastefania.github.io/DEV003-data-lovers/src/index.html' href1='https://github.com/SaraStefania/DEV003-data-lovers' titulo='Data Lovers' src='public/assets/datalovers.png'></Tarjetas>
-          <Tarjetas href2='SaraStefania.github.io/DEV003-card-validation/src/index.html' href1='https://github.com/SaraStefania/DEV003-card-validation' titulo='Card Validation' src='public/assets/cardValidation.png'></Tarjetas>
+          <Tarjetas href2='https://sarastefania.github.io/DEV003-data-lovers/src/index.html' href1='https://github.com/SaraStefania/DEV003-data-lovers' titulo='Data Lovers' src='/assets/datalovers.png'></Tarjetas>
+          <Tarjetas href2='SaraStefania.github.io/DEV003-card-validation/src/index.html' href1='https://github.com/SaraStefania/DEV003-card-validation' titulo='Card Validation' src='/assets/cardValidation.png'></Tarjetas>
         </div>
     </div>
   )
