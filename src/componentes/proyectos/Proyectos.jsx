91176d8ff90f6a62ccf17171f8ada3e5fac8e7f5
sara040616 <sara.sierraramos@gmail.com>
2023-08-25 22:11:52 -0500

    Agregando url del repositorio de portafolio

--- src/componentes/proyectos/Proyectos.jsx
+++ src/componentes/proyectos/Proyectos.jsx
@@ -14,7 +14,7 @@ export default function Proyectos() {
         <Subtitulo>Mis proyectos</Subtitulo>
         <Titulo>Proyectos</Titulo>
         <div className='d-flex flex-wrap proyectos'>
-          <Tarjetas href2='https://portafolio-theta-gilt.vercel.app/' href1='https://github.com/SaraStefania' titulo='Portafolio' src='/assets/portafolio.png'></Tarjetas>
+          <Tarjetas href2='https://portafolio-theta-gilt.vercel.app/' href1='https://github.com/SaraStefania/Portafolio' titulo='Portafolio' src='/assets/portafolio.png'></Tarjetas>
           <Tarjetas href2='https://dev-003-burger-queen-api-client-one.vercel.app/' href1='https://github.com/SaraStefania/DEV003-burger-queen-api-client' titulo='Burguer queen' src='/assets/burguerImagen.png'></Tarjetas>
           <Tarjetas href2='www.npmjs.com/package/md-links-minder' href1='https://github.com/SaraStefania/DEV003-md-links' titulo='Md Links' src='/assets/mdLinks.png'></Tarjetas>
           <Tarjetas href2='https://sarastefania.github.io/DEV003-data-lovers/src/index.html' href1='https://github.com/SaraStefania/DEV003-data-lovers' titulo='Data Lovers' src='/assets/datalovers.png'></Tarjetas>
